af6077cc72c04cc2e817e95d11eaa41f8c3373e7
Brent Feuerbacher <feuerbacherb@gmail.com>
2024-04-05 14:49:34 -0500

    BACONgit add -A!

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to the database
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log("Connected to the election database.");
+});
+
 // default response for any other requires (Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// start server after DB connection
+db.on("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
